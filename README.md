7a11a6f0acda93430f7ca59997d227dcc40ce07f
ladoger <ladogerdefi@gmail.com>
2023-12-08 10:58:04 +0700

    readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Ollama-Python-Client
 Python script to access Ollama APIs
+
+
+## How to
+1. To use this script, you need to install [Ollama](https://ollama.ai/): `curl https://ollama.ai/install.sh | sh`
+2. Then you need to download a model in Ollama (using [mistral](https://ollama.ai/library/mistral) as an example): `ollama run mistral`
+3. Then you execute `completion.py` or `chat.py`.
--- completion.py
+++ completion.py
@@ -1,12 +1,24 @@
+import json
 import requests
+
+
+# Set system prompt here
+system_prompt = """Be concise!"""
+
+# Set user prompt here
+user_prompt = """Teach me yoga.
+"""
+
 
 url = 'http://localhost:11434/api/generate'
 data = {
     "model": "mistral",
-    "system": "You always reply only with the word 'doge', and nothing else.",
-    "prompt": "haha"
+    "system": system_prompt,
+    "prompt": user_prompt,
+    "stream": False
 }
 
 response = requests.post(url, json=data)
+response_data = json.loads(response.text)
 
-print(response.text)
+print(response_data["response"])
