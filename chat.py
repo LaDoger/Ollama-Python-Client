be87880a6c67752cced96650e35130b2b72fea9d
ladogerdefi <ladogerdefi@gmail.com>
2023-12-13 13:20:10 +0700

    chat API

--- chat.py
+++ chat.py
@@ -1,18 +1,32 @@
-# The chat API is not live yet as of 8 Dec 2023. Please be patient.
-
+import json
 import requests
+
+
+# Choose the model you want to use. By default it's "mistral"
+model = "mistral"
+
+# Set system prompt here
+system_prompt = """reply only with the word 'dogecoin'"""
 
 url = 'http://localhost:11434/api/chat'
 data = {
-    "model": "mistral",
+    "model": model,
     "messages": [
         {
+            
+            "role": "system",
+            "content": system_prompt
+        },
+        {
+
             "role": "user",
-            "content": "reply only with the word 'dogecoin'"
+            "content": "hi"
         }
-    ]
+    ],
+    "stream": False
 }
 
 response = requests.post(url, json=data)
+response_data = json.loads(response.text)
 
-print(response.text)
+print(response_data["message"]["content"])
